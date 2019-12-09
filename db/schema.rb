9632414679529677253d0ecbb92cd154a9b24b79
Hamza Ben M Barek <hamza.ben_mbarek@edu.escpeurope.eu>
2019-12-09 18:21:09 +0100

    added our fields to the User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_09_162914) do
+ActiveRecord::Schema.define(version: 2019_12_09_165756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,18 @@ ActiveRecord::Schema.define(version: 2019_12_09_162914) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.string "photo"
+    t.string "city"
+    t.date "birthday"
+    t.string "gender"
+    t.string "sex_pref"
+    t.numrange "age_pref"
+    t.integer "distance"
+    t.integer "diagnosis_year"
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
