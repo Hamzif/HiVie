2d9cabb5dbb69dbff678ab885044348e492b2269
Hamza Ben M Barek <hamza.ben_mbarek@edu.escpeurope.eu>
2019-12-10 12:29:30 +0100

    created the view and the controller method for the components

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/components', to: 'pages#components' # temporary route for displaying components
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
