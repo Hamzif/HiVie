14fff22f0c09b420feb62ace4d19060a4ed4bf22
Elise Navarro <elise.marie.navarro@gmail.com>
2019-12-11 16:06:49 +0100

    Routes: Added resources: profiles to Routes.rb file for Edit/Update actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
    registrations: 'users/registrations'
   }
+  resources :profiles, only: [:edit, :update]
   root to: 'pages#home'
   get '/components', to: 'pages#components' # temporary route for displaying components
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
