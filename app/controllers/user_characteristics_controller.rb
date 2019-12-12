6e46daed7b918438de8c4fa03db2dd88becff0af
Ivana Piljic <piljic.ivana@gmail.com>
2019-12-12 17:08:50 +0100

    Pills working

--- app/controllers/user_characteristics_controller.rb
+++ app/controllers/user_characteristics_controller.rb
@@ -1,20 +1,21 @@
 class UserCharacteristicsController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:home, :update, :new]
 
-
   def new
-    @user_characteristic = UserCharacteristic.new
   end
 
   def create
+    @characteristic_id = params[:characteristic_id]
+    @user_characteristic = UserCharacteristic.create(user: current_user, characteristic_id: @characteristic_id)
 
+    render :new
   end
 
-  def edit
+  def destroy
+    @user_characteristic = UserCharacteristic.find(params[:id])
+    @characteristic_id = @user_characteristic.characteristic_id
+    @user_characteristic.destroy
 
-  end
-
-  def update
-
+    render :new
   end
 end
