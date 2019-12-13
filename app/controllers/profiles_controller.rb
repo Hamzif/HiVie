d24fc766c06504f905e5b17f383343df55a0863b
Elise Navarro <elise.marie.navarro@gmail.com>
2019-12-13 13:05:21 +0100

    Added @Potential_Matches logic to update method to redirect userto potential match

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,7 +7,17 @@ class ProfilesController < ApplicationController
     @user = User.find(params[:id])
     @user.update(user_params)
     @user.save
-    # redirect_to match show page
+
+    @potential_matches = []
+    if current_user
+      @potential_matches << User.where.not(id: current_user.id)
+                                .where(gender: current_user.sex_pref)
+                                .where(sex_pref: current_user.gender)
+                                #.where((min_age >= current_user.age) && (max_age >= current_user.age))
+      @user = @potential_matches[0].sample
+    end
+
+    redirect_to user_path(@potential_matches.sample)
   end
 
   private
