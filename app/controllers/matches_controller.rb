c1216c22a8fd8301ea7e02f1d8236534c9fe1fc6
Hamza Ben M Barek <hamza.ben_mbarek@edu.escpeurope.eu>
2019-12-13 12:17:40 +0100

    changed the names of match seeds (all match_1 before)

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,16 @@
 class MatchesController < ApplicationController
 
-  def create
+  def new
+    @match = Match.new
+  end
 
+  def create
+    @match = Match.new(match_params)
+  end
+
+  private
+
+  def match_params
+    params.require(:matches).permit(:user_one_id, :user_two_id)
   end
 end
