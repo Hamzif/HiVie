7bd8db764c43e882a8d3f1f5273a98e56418ba1f
Hamza Ben M Barek <hamza.ben_mbarek@edu.escpeurope.eu>
2019-12-16 16:51:45 +0100

    delete function works on the chat navbar

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,15 +1,15 @@
 class MatchesController < ApplicationController
-  layout 'message_layout', only: [:index]
   def index
     # list of all matches from which, user can create single chat conversation
+
     matches1 = Match.where(user_one_id: current_user.id).where(status: "validated")
     matches2 = Match.where(user_two_id: current_user.id).where(status: "validated")
     @matches = matches1 + matches2
   end
 
-  def index_requests
-    @bookings = Booking.where(user_id: current_user.id)
-  end
+  # def index_requests
+  #   @bookings = Booking.where(user_id: current_user.id)
+  # end
 
   def new
     @match = Match.new
@@ -56,5 +56,11 @@ class MatchesController < ApplicationController
                               # .where((min_age >= current_user.age) && (max_age >= current_user.age))
     # .where(current_user.age_pref.includes)
     @next_user = @potential_matches[0].sample
+  end
+
+  def destroy
+    @match = Match.find(params[:id])
+    @match.destroy
+    redirect_to matches_path
   end
 end
