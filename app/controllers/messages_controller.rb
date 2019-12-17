0f4b3ec4207f7849055af008028be3711e0f987a
Humbeline de Marcillac <humbeline.dm@gmail.com>
2019-12-17 11:08:29 +0100

    added the right color to messgaes, and date, and the right size to the message box

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,24 @@
 class MessagesController < ApplicationController
   def index
-    @messages = Message.where(match: params[:match_id])
+    @messages = Message.where(match: params[:match_id]).group(:id, :created_at)
+    @message = Message.new
+    @match = Match.find(params[:match_id])
+    # raise
   end
 
   def create
-    @message = Message.new(params[:content])
-    @message.match = Match.find(params[:match_id])
-    @message.user = current_user.id
+    @message = Message.new(messages_params)
+    @match = Match.find(params[:match_id])
+    @message.match = @match
+    @message.user = current_user
     @message.save
-    redirect_to match_messages(params[:match_id])
+    redirect_to match_messages_path(@match)
+  end
+
+  private
+
+  def messages_params
+    params.require(:message).permit(:content)
   end
 
 end
